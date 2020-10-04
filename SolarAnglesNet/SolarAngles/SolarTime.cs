bfc4f6ff76f6080ec368b0687d8df3e2d23d4c13
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-04 22:12:50 +0200

    Added conversion from solarTime to localTime

--- SolarAnglesNet/SolarAngles/SolarTime.cs
+++ SolarAnglesNet/SolarAngles/SolarTime.cs
@@ -27,14 +27,34 @@ namespace SolarAngles
         /// <param name="timeDelta">Time difference to Greenwich Mean Time</param>
         public static DateTime GetSolarTime(DateTime localTime, double longitude, TimeSpan timeDelta)
         {
-            double localStandardMeridian = GetLocalStandardMeridian(timeDelta);
-            
-            double B = CalculationAbbreviations.DayOnCircle(localTime.DayOfYear);
-            double equationOfTime = EquationOfTime(B);
-            
-            double timeDeltaMinutes = 4 * (localStandardMeridian - longitude) + equationOfTime;
+            double timeDeltaMinutes = SolarTimeDeltaMinutes(localTime.DayOfYear, longitude, timeDelta);
 
             return localTime + TimeSpan.FromMinutes(timeDeltaMinutes);
+        }
+
+        /// <summary>
+        /// Transform solar to local time
+        /// See Equation 1.5.2 on page 11.
+        /// </summary>
+        /// <param name="localTime">Local time</param>
+        /// <param name="longitude">Longitude of location</param>
+        /// <param name="timeDelta">Time difference to Greenwich Mean Time</param>
+        public static DateTime GetLocalTime(DateTime solarTime, double longitude, TimeSpan timeDelta)
+        {
+            double timeDeltaMinutes = SolarTimeDeltaMinutes(solarTime.DayOfYear, longitude, timeDelta);
+
+            return solarTime - TimeSpan.FromMinutes(timeDeltaMinutes);
+        }
+
+        private static double SolarTimeDeltaMinutes(int dayOfYear, double longitude, TimeSpan timeDelta)
+        {
+            double localStandardMeridian = GetLocalStandardMeridian(timeDelta);
+
+            double B = CalculationAbbreviations.DayOnCircle(dayOfYear);
+            double equationOfTime = EquationOfTime(B);
+
+            double timeDeltaMinutes = 4 * (localStandardMeridian - longitude) + equationOfTime;
+            return timeDeltaMinutes;
         }
     }
 }
