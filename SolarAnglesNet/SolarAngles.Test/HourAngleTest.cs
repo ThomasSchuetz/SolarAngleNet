0fe3d22e969ec285dabb871cff3bc14dc9c6caea
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-04 22:08:10 +0200

    Added calculation of hour angle (omega)

---
+++ SolarAnglesNet/SolarAngles.Test/HourAngleTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using static Converter.RadianDegreeConverter;
+
+namespace SolarAngles.Test
+{
+    [TestFixture]
+    public class HourAngleTest
+    {
+        [Test]
+        public void HourAngleAtSolarNoonIsZero()
+        {
+            var result = HourAngle.GetHourAngle(new DateTime(2020, 1, 1, 12, 0, 0));
+            Assert.AreEqual(0.0, result);
+        }
+
+        [Test]
+        public void HourAngleInAfternoonIsPositive()
+        {
+            var resultRadians = HourAngle.GetHourAngle(new DateTime(2020, 1, 1, 18, 0, 0));
+            var resultDegrees = resultRadians.FromRadiansToDegree();
+            Assert.AreEqual(90.0, resultDegrees);
+        }
+
+        [Test]
+        public void HourAngleInMorningIsNegative()
+        {
+            var resultRadians = HourAngle.GetHourAngle(new DateTime(2020, 1, 1, 6, 0, 0));
+            var resultDegrees = resultRadians.FromRadiansToDegree();
+            Assert.AreEqual(-90.0, resultDegrees);
+        }
+    }
+}
---
+++ SolarAnglesNet/SolarAngles/HourAngle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static Converter.RadianDegreeConverter;
+
+namespace SolarAngles
+{
+    public static class HourAngle
+    {
+        /// <summary>
+        /// Hour angle (omega): The angular displacement of the sun east or west of the 
+        /// local meridian due to rotation of the earth on its axis at 15 degrees per hour; 
+        /// morning negative, afternoon positive
+        /// Confirm page 13
+        /// </summary>
+        /// <param name="solarTime"></param>
+        public static double GetHourAngle(DateTime solarTime)
+        {
+            var noon = new DateTime(solarTime.Year, solarTime.Month, solarTime.Day, 12, 0, 0);
+            var timeDelta = solarTime - noon;
+
+            var hourAngleDegrees = 15 * timeDelta.TotalHours;
+            return hourAngleDegrees.FromDegreeToRadians();
+        }
+    }
+}
