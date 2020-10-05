b04e25bb74e3152e6fa2f7d042684f6c252c840f
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-05 12:57:51 +0200

    Added wrapper class for all previously developed static angles

---
+++ SolarAnglesNet/SolarAngles.Test/SolarAnglesTest.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using static Converter.RadianDegreeConverter;
+
+namespace SolarAngles.Test
+{
+    [TestFixture]
+    public class SolarAnglesTest
+    {
+        private SolarAngles MadisonWisconsinFlatSurface()
+            => MadisonWisconsinTiltedSurface(0, 0);
+        private SolarAngles MadisonWisconsinTiltedSurface(double beta, double gamma)
+            => new SolarAngles(new Location(43, 0), new TiltedSurface(beta, gamma));
+
+        [Test]
+        public void SolarAnglesFacadeHourAngleAtSolarNoonIsZero()
+        {
+            var solarAngles = MadisonWisconsinFlatSurface();
+            var result = solarAngles.GetHourAngle(new DateTime(2020, 1, 1, 12, 0, 0));
+            Assert.AreEqual(0.0, result);
+        }
+
+        [Test]
+        public void SolarAnglesFacadeHourAngleInAfternoonIsPositive()
+        {
+            var solarAngles = MadisonWisconsinFlatSurface();
+            var resultRadians = solarAngles.GetHourAngle(new DateTime(2020, 1, 1, 18, 0, 0));
+            var resultDegrees = resultRadians.FromRadiansToDegree();
+            Assert.AreEqual(90.0, resultDegrees);
+        }
+
+        [Test]
+        public void SolarAnglesFacadeHourAngleInMorningIsNegative()
+        {
+            var solarAngles = MadisonWisconsinFlatSurface();
+            var resultRadians = solarAngles.GetHourAngle(new DateTime(2020, 1, 1, 6, 0, 0));
+            var resultDegrees = resultRadians.FromRadiansToDegree();
+            Assert.AreEqual(-90.0, resultDegrees);
+        }
+
+        [Test]
+        public void SolarAnglesFacadeTestIncidenceAngleForTiltedSurface()
+        {
+            var solarAngles = MadisonWisconsinTiltedSurface(45, 15);
+            var solarTime = new DateTime(2020, 2, 13, 10, 30, 0);
+
+            var resultRadian = solarAngles.GetIncidenceAngle(solarTime);
+
+            var resultDegree = resultRadian.FromRadiansToDegree();
+            Assert.AreEqual(35, resultDegree, 0.5);
+        }
+
+
+        [Test]
+        public void SolarAnglesFacadeTestZenithAngleFebruary13()
+            => TestZenithAngle(new DateTime(2020, 2, 13, 9, 30, 0), 66.5);
+
+        [Test]
+        public void SolarAnglesFacadeTestZenithAngleJuly1()
+            => TestZenithAngle(new DateTime(2020, 7, 1, 18, 30, 0), 79.6);
+
+        private bool TestZenithAngle(DateTime solarTime, double expectedResultDegree)
+        {
+            var solarAngles = MadisonWisconsinFlatSurface();
+            var resultRadian = solarAngles.GetZenithAngle(solarTime);
+
+            Assert.AreEqual(expectedResultDegree, resultRadian.FromRadiansToDegree(), 0.1);
+            return true;
+        }
+    }
+}
---
+++ SolarAnglesNet/SolarAngles/SolarAngles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static Converter.RadianDegreeConverter;
+
+namespace SolarAngles
+{
+    /// <summary>
+    /// This class presents a collection of functions explaind in the book
+    ///     "Solar Engineering of Thermal Processes" (4th edition, 2013) 
+    ///     by John A. Duffie and William A. Beckman
+    /// </summary>
+    public class SolarAngles
+    {
+        private readonly Configuration config;
+
+        public Location Location { get; }
+        public TiltedSurface TiltedSurface { get; }
+
+        /// <summary>
+        /// Constructor for only setting the location. 
+        /// A flat horizontal surface will be assumed.
+        /// </summary>
+        /// <param name="location">Location defining latitude and longitude</param>
+        public SolarAngles(Location location) 
+            : this(location, TiltedSurface.GetFlatHorizontalSurface())
+        { }
+
+        public SolarAngles(Location location, TiltedSurface tiltedSurface)
+        {
+            config = Configuration.Config;
+
+            Location = location;
+            TiltedSurface = tiltedSurface;
+        }
+
+        public double GetHourAngle(DateTime solarTime)
+            => HourAngle.GetHourAngle(solarTime);
+
+        public double GetIncidenceAngle(DateTime solarTime) 
+            => GetIncidenceAngle(solarTime, TiltedSurface);
+
+        public double GetZenithAngle(DateTime solarTime)
+            => GetIncidenceAngle(solarTime, TiltedSurface.GetFlatHorizontalSurface());
+
+        private double GetIncidenceAngle(DateTime solarTime, TiltedSurface tiltedSurface)
+        {
+            var declinationAngle = config.DeclinationAngle.DeclinationAngle(solarTime.DayOfYear);
+            var hourAngle = GetHourAngle(solarTime);
+
+            Console.WriteLine($"Declination angle: {declinationAngle.FromRadiansToDegree()}");
+            Console.WriteLine($"Hour angle: {hourAngle.FromRadiansToDegree()}");
+
+            return IncidenceAngle.GetIncidenceAngle(tiltedSurface,
+                Location.Latitude.FromDegreeToRadians(),
+                declinationAngle,
+                hourAngle);
+        }
+    }
+}
