74d7b4f777fe8e9c579d88348500182b3d68e6d3
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-05 11:22:28 +0200

    Made Zenith angle a special case of incidence angle

--- SolarAnglesNet/SolarAngles/ZenithAngle.cs
+++ SolarAnglesNet/SolarAngles/ZenithAngle.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace SolarAngles
+﻿namespace SolarAngles
 {
     public static class ZenithAngle
     {
@@ -9,7 +7,6 @@ namespace SolarAngles
         /// that is, the angle of incidence of beam radiation on a horizontal surface; 
         /// 0 <= thetaZ <= 90. If thetaZ > 90, the sun is below the horizon.
         /// All inputs are required in radian.
-        /// Equation 1.6.5 on page 15
         /// </summary>
         /// <param name="latitude">Latitude in radian (phi)</param>
         /// <param name="declinationAngle">Declination angle in radian (delta)</param>
@@ -18,19 +15,9 @@ namespace SolarAngles
             double declinationAngle,
             double hourAngle)
         {
-            // Abbreviations
-            var cosPhi = Math.Cos(latitude);
-            var sinPhi = Math.Sin(latitude);
-            var cosDelta = Math.Cos(declinationAngle);
-            var sinDelta = Math.Sin(declinationAngle);
-            var cosOmega = Math.Cos(hourAngle);
-
-            var cosThetaZ = cosPhi * cosDelta * cosOmega + sinPhi * sinDelta;
-
-            // Ensure that cos(ThetaZ) is positive, so that ThetaZ is between 0 and pi/2
-            cosThetaZ = Math.Max(0, cosThetaZ);
-
-            return Math.Acos(cosThetaZ);
+            // Zenith angle is the incidence angle onto a horizontal surface.
+            var horizontalSurface = new TiltedSurface(0, 0);
+            return IncidenceAngle.GetIncidenceAngle(horizontalSurface, latitude, declinationAngle, hourAngle);
         }
     }
 }
