670094af4737e36ea9c0f1ccb6ee04269a994f9e
Thomas Schuetz <thomas.schuetz@eon.com>
2020-10-08 22:14:18 +0200

    Added constant for anglesPerHour and method for transforming hour angle to time span

--- SolarAnglesNet/SolarAngles/HourAngle.cs
+++ SolarAnglesNet/SolarAngles/HourAngle.cs
@@ -5,6 +5,8 @@ namespace SolarAngles
 {
     public static class HourAngle
     {
+        private const double anglesPerHour = 15;
+
         /// <summary>
         /// Hour angle (omega): The angular displacement of the sun east or west of the 
         /// local meridian due to rotation of the earth on its axis at 15 degrees per hour; 
@@ -17,8 +19,16 @@ namespace SolarAngles
             var noon = new DateTime(solarTime.Year, solarTime.Month, solarTime.Day, 12, 0, 0);
             var timeDelta = solarTime - noon;
 
-            var hourAngleDegrees = 15 * timeDelta.TotalHours;
+            var hourAngleDegrees = anglesPerHour * timeDelta.TotalHours;
             return hourAngleDegrees.FromDegreeToRadians();
+        }
+
+        /// <summary>Transforms an hour angle to a time span.</summary>
+        /// <param name="hourAngle">Hour angle in radian</param>
+        public static TimeSpan GetTimeSpanFromHourAngle(double hourAngle)
+        {
+            var hourAngleDegrees = hourAngle.FromRadiansToDegree();
+            return TimeSpan.FromHours(hourAngleDegrees / anglesPerHour);
         }
     }
 }
